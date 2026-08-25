a4dc0064168141d6e8d29fb946f763d1e5d93150
20520-ops <20520@songgok.sen.hs.kr>
2026-08-25 23:51:24 +0900

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -1,47 +1,19 @@
-import smtplib
+name: CGV Ticket Auto Checker
 
+on:
+  workflow_dispatch:
 
+jobs:
+  check-ticket:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout code
+        uses: actions/checkout@v3
 
+      - name: Set up Python
+        uses: actions/setup-python@v4
+        with:
+          python-version: '3.10'
 
-
-from email.mime.text import MIMEText
-
-EMAIL_USER = "3branwell@gmail.com"
-
-
-EMAIL_PASS = "jskofjyzxvrfcyqi"
-
-def send_email():
-    
-    
-    msg = MIMEText("테스트 메일 전송 성공했습니다!")
- 
-    
-    msg['Subject'] = "🚨 [테스트] CGV 용산 IMAX 알림 테스트!"
-    
-    
-    msg['From'] = EMAIL_USER
-    
-    
-    msg['To'] = EMAIL_USER
-
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    
-    
-    server.starttls()
-    
-    
-    server.login(EMAIL_USER, EMAIL_PASS)
-    
-    
-    server.sendmail(EMAIL_USER, EMAIL_USER, msg.as_string())
-    
-    
-    server.quit()
-    
-    
-    
-    print("메일 전송 성공!")
-
-if __name__ == "__main__":
-    send_email()
+      - name: Run Crawler
+        run: python crawler.py
