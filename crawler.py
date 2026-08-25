b0f1de740a378b20db3d5b72b1460a26a3a298ec
20520-ops <20520@songgok.sen.hs.kr>
2026-08-25 23:47:22 +0900

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -1,20 +1,46 @@
 import smtplib
+
+
+
+
+
 from email.mime.text import MIMEText
 
 EMAIL_USER = "3branwell@gmail.com"
+
+
 EMAIL_PASS = "jskofjyzxvrfcyqi"
 
 def send_email():
+    
+    
     msg = MIMEText("테스트 메일 전송 성공했습니다!")
+ 
+    
     msg['Subject'] = "🚨 [테스트] CGV 용산 IMAX 알림 테스트!"
+    
+    
     msg['From'] = EMAIL_USER
+    
+    
     msg['To'] = EMAIL_USER
 
     server = smtplib.SMTP('smtp.gmail.com', 587)
+    
+    
     server.starttls()
+    
+    
     server.login(EMAIL_USER, EMAIL_PASS)
+    
+    
     server.sendmail(EMAIL_USER, EMAIL_USER, msg.as_string())
+    
+    
     server.quit()
+    
+    
+    
     print("메일 전송 성공!")
 
 if __name__ == "__main__":
