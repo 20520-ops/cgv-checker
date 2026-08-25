da972ae44035aebaa0082a5372a5eb10988f0af1
20520-ops <20520@songgok.sen.hs.kr>
2026-08-25 23:32:34 +0900

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -2,11 +2,10 @@ import smtplib
 from email.mime.text import MIMEText
 
 EMAIL_USER = "3branwell@gmail.com"
-# 아래에 새로 발급받은 16자리 앱 비밀번호를 넣으세요
-EMAIL_PASS = "jsko fjyz xvrf cyqi"
+EMAIL_PASS = "jskofjyzxvrfcyqi"
 
 def send_email():
-    msg = MIMEText("테스트 메일입니다.")
+    msg = MIMEText("테스트 메일 전송에 성공했습니다.")
     msg['Subject'] = "🚨 [테스트] CGV 용산 IMAX 알림 테스트!"
     msg['From'] = EMAIL_USER
     msg['To'] = EMAIL_USER
@@ -16,6 +15,7 @@ def send_email():
     server.login(EMAIL_USER, EMAIL_PASS)
     server.sendmail(EMAIL_USER, EMAIL_USER, msg.as_string())
     server.quit()
+    print("메일 전송 성공!")
 
 if __name__ == "__main__":
     send_email()
