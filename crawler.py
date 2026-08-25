8ff10dd7e27a8a79ef7a23c83b871673a8a65a71
20520-ops <20520@songgok.sen.hs.kr>
2026-08-25 22:59:16 +0900

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -1,6 +1,5 @@
-# 기존: if check_open():
-# 테스트용 (무조건 발송):
-if True:
+# 맨 아래 코드 부분 수정
+if True:  # check_open() 대신 True로 변경
     send_email(
         f"🚨 [테스트] CGV 용산 IMAX 알림 테스트!",
         f"이 메일이 도착했다면 알림 설정이 완벽하게 완료된 것입니다!"
